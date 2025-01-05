e2cf434092c9c65effe4e7ebb0bb478a3247d17d
Paul-Dieter <pauldieter.brandt@yahoo.com>
2025-01-05 19:15:46 +0200

    Added js for loading bar delay and typing animations

--- scritp.js
+++ scritp.js
@@ -1,3 +1,5 @@
+
+/* Timeline */
 const targets = document.querySelectorAll(".timeline ol li");
 const threshold = 0.5;
 const ANIMATED_CLASS = "in-view";
@@ -18,3 +20,14 @@ const observer = new IntersectionObserver(callback, { threshold });
 for (const target of targets) {
   observer.observe(target);
 }
+
+/* Loading bar delay */
+document.addEventListener("DOMContentLoaded", function () {
+  // Set the delay in milliseconds (e.g., 5000ms = 5 seconds)
+  const delay = 5000;
+
+  // Show the loader after the delay
+  setTimeout(() => {
+      document.querySelector(".loader").style.display = "block";
+  }, delay);
+});
