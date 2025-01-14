49279c54a7e196f9e90d933814cafe03357e0f8d
Paul-Dieter <pauldieter.brandt@yahoo.com>
2025-01-14 17:23:52 +0200

    many changes

--- script.js
+++ script.js
@@ -15,3 +15,30 @@ navItems.forEach(item => {
         mobileMenuBtn.classList.remove('active'); // Optionally remove 'active' from the burger icon
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');
+    
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('is-visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    sections.forEach(section => {
+        section.classList.add('fade-in-section');
+        observer.observe(section);
+    });
+});
+
+//draggable carousel effect
+
